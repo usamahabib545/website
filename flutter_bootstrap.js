ae2944ad71fc020574652e4131b52171447a0621
Usama Habib <usama.dstech@gmail.com>
2025-08-12 19:13:35 +0500

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3241749051"
+    serviceWorkerVersion: "628647386"
   }
 });
